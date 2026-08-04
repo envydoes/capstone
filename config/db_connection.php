0ffcdc27e1c21282b4539d09c482e427d5f22905
Madulid Joshua M. <jeroldmadulid@gmail.com>
2026-08-04 17:05:09 +0800

    Fix database connection for Docker networking

--- config/db_connection.php
+++ config/db_connection.php
@@ -1,14 +1,9 @@
 <?php
-
-$host = "localhost";
+$host = "o7jpqmin0zgconui4xtnfju6";
 $user = "root";
-$password = "";
+$password = "UKkJ05DHQDMMMOxFEUI5f1HJGVj8Vb5gfJAEvAESTGCVWDtFEGb42qX67AxGUXvj";
 $database = "sumeste_db";
-
 $conn = mysqli_connect($host, $user, $password, $database);
-
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-?>
