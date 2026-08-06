a0f6488846ea9720dee7e9a0cd59c39a53d64a8b
Madulid Joshua M. <jeroldmadulid@gmail.com>
2026-08-06 16:04:15 +0800

    Replace unrecoverable mojibake copyright/heart characters with HTML entities in nonresidentLanding.php footer

--- nonResident/nonresidentLanding.php
+++ nonResident/nonresidentLanding.php
@@ -798,7 +798,7 @@ function tagColor(string $tag): array {
         </div>
       </div>
       <div class="text-center mt-6 text-green-500 text-sm">
-        � 2026 SumEste Portal. All Rights Reserved. Made with ?? for <?= e($siteSettings['barangay_name']) ?>.
+        &copy; 2026 SumEste Portal. All Rights Reserved. Made with &hearts; for <?= e($siteSettings['barangay_name']) ?>.
       </div>
     </div>
   </footer>
