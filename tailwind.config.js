e9b25beb461d675e1476a9c85e89154d1db411ca
Madulid Joshua M. <jeroldmadulid@gmail.com>
2026-08-15 14:06:29 +0800

    Replace Tailwind CDN with local CLI build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./**/*.php",
-    "./**/*.html",
-    "./**/*.js",
+    "./*.{html,php,js}",          // Files in root folder
+    "./**/*.{html,php,js}",       // Files in subfolders
+    "!./node_modules/**/*",       // EXCLUDE node_modules
+    "!./vendor/**/*"              // EXCLUDE PHP vendor folder (if using Composer)
   ],
   theme: {
     extend: {},
